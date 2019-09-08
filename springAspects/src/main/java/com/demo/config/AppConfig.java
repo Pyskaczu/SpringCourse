8c3cb10132da9edd70e509c7778e05224f82be09
Michal Babiarz <michal.babiarz84@gmail.com>
2019-09-08 19:36:29 +0200

    Implemets aspect chapter

--- springAspects/src/main/java/com/demo/config/AppConfig.java
+++ springAspects/src/main/java/com/demo/config/AppConfig.java
@@ -5,15 +5,46 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-import com.demo.vehicles.Car;
+import com.demo.sportsEquipment.GolfClubDesignators;
+import com.demo.vehicles.CarAdvice;
+import com.demo.vehicles.CarDesignators;
+import com.demo.vehicles.CarPointcut;
+import com.demo.vehicles.offroad.DirtBikeDesignators;
+import com.demo.vehicles.offroad.FourWheelerDesignators;
 
 @Configuration
 @ComponentScan("com.demo.aop")
 @EnableAspectJAutoProxy
 public class AppConfig {
 
-	@Bean(name="car")
-	public Car getCar() {
-		return new Car();
+	@Bean(name="carAdvice")
+	public CarAdvice getCarAdvice() {
+		return new CarAdvice();
 	}
+	
+	@Bean(name="carPointcut")
+	public CarPointcut getCarPointcut() {
+		return new CarPointcut();
+	}
+	
+	@Bean(name="carDesignators")
+	public CarDesignators getCarDesignators() {
+		return new CarDesignators();
+	}
+	
+	@Bean(name="dirtBikeDesignators")
+	public DirtBikeDesignators getDirtBikeDesignators() {
+		return new DirtBikeDesignators();
+	}
+	
+	@Bean(name="fourWheelerDesignators")
+	public FourWheelerDesignators getFourWheelerDesignators() {
+		return new FourWheelerDesignators();
+	}
+	
+	@Bean(name="golfClubDesignators")
+	public GolfClubDesignators getGolfClubDesignators() {
+		return new GolfClubDesignators();
+	}
+	
 }
--- springAspects/src/main/java/com/demo/vehicles/aspectsApp.java
+++ springAspects/src/main/java/com/demo/vehicles/aspectsApp.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import com.demo.config.AppConfig;
+import com.demo.sportsEquipment.GolfClubDesignators;
+import com.demo.vehicles.offroad.DirtBikeDesignators;
+import com.demo.vehicles.offroad.FourWheelerDesignators;
 
 public class aspectsApp {
 
@@ -12,13 +15,39 @@ public class aspectsApp {
 		//ApplicationContext appContext = new FileSystemXmlApplicationContext("src\\main\\java\\com\\demo\\config\\appContext.xml");
 		ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
 		
-		
-		Car car = appContext.getBean("car", Car.class);
+		System.out.println("******************** Advice **************************************");
+		CarAdvice carAdvice = appContext.getBean("carAdvice", CarAdvice.class);
 
-		String result = car.drive(112);
+		String result = carAdvice.drive(112);
 		System.out.println("MAIN: " + result);
 		
-		((FileSystemXmlApplicationContext) appContext).close();
+		System.out.println("******************** Pointcut ************************************");
+		
+		CarPointcut carPointcut = appContext.getBean("carPointcut", CarPointcut.class);
+		carPointcut.drive(30);
+		carPointcut.drift();
+		carPointcut.drive("Hello");
+		carPointcut.stop();
+		
+		System.out.println("******************** Designators *********************************");
+		
+		CarDesignators carDesignators = appContext.getBean("carDesignators", CarDesignators.class);
+		DirtBikeDesignators dirtBikeDesignators = appContext.getBean("dirtBikeDesignators", DirtBikeDesignators.class);
+		FourWheelerDesignators fourWheelerDesignators = appContext.getBean("fourWheelerDesignators", FourWheelerDesignators.class);
+		GolfClubDesignators golfClubDesignators = appContext.getBean("golfClubDesignators", GolfClubDesignators.class);
+		
+		carDesignators.drive();
+		carDesignators.drive(10);
+		carDesignators.drive(10, 20);
+		carDesignators.drive("Shooooouting");
+		dirtBikeDesignators.drive();
+		fourWheelerDesignators.drive();
+		golfClubDesignators.drive();
+		
+		
+		
+		//((FileSystemXmlApplicationContext) appContext).close();
+		((AnnotationConfigApplicationContext) appContext).close();
 		
 	}
 
